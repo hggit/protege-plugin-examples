ff165fdc47695238eae3954aac3192e32b1a1d6f
Ally Brown <Ally Brown@DESKTOP-ALLY.minerva.home>
2018-06-20 16:01:51 -0700

    .

--- src/main/java/edu/stanford/bmir/protege/examples/view/DisplayAttributes.java
+++ src/main/java/edu/stanford/bmir/protege/examples/view/DisplayAttributes.java
@@ -1,22 +1,31 @@
 package edu.stanford.bmir.protege.examples.view;
 
 import java.awt.event.ActionListener;
+import java.util.*;
+import java.awt.BorderLayout;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+
 
 import org.protege.editor.owl.model.OWLModelManager;
 import org.protege.editor.owl.model.event.EventType;
 import org.protege.editor.owl.model.event.OWLModelManagerListener;
 
 import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLAxiom;
+import org.semanticweb.owlapi.model.OWLClassAxiom;
+import org.semanticweb.owlapi.model.OWLClassExpression;
 
 public class DisplayAttributes extends JPanel {
 
     //private JButton refreshButton = new JButton("Refresh");
 
     private JLabel textComponent = new JLabel();
+    JTextArea textArea;
 
     private OWLModelManager modelManager;
 
@@ -39,6 +48,12 @@ public class DisplayAttributes extends JPanel {
         
         add(textComponent);
         //add(refreshButton);
+        
+        textArea = new JTextArea(5, 30);
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        //setPreferredSize(new Dimension(450, 110));
+        add(scrollPane, BorderLayout.CENTER);
+
     }
     
     public void dispose() {
@@ -50,9 +65,22 @@ public class DisplayAttributes extends JPanel {
         /*int count = modelManager.getActiveOntology().getClassesInSignature().size();
         if (count == 0) {
             count = 1;  // owl:Thing is always there.
-        }*/
-    	
-    	
-        if(selectedClass!=null) textComponent.setText("Selected Class : " + selectedClass.getIRI().getFragment());
+        }*/    	
+    	       
+        String s="";
+        Set<OWLClassAxiom> tempAx=modelManager.getActiveOntology().getAxioms(selectedClass);
+        for(OWLClassAxiom ax: tempAx){
+            //for(OWLClassExpression nce:ax.getNestedClassExpressions())
+                //if(nce.getClassExpressionType()!=ClassExpressionType.OWL_CLASS)
+        	//Set<OWLClass> signature = ax.getClassesInSignature();
+                    s+=ax+"\n";
+        }
+        
+        if(selectedClass!=null)
+        	{
+        		textComponent.setText("Selected Class : " + selectedClass.getIRI().getFragment());
+        		textArea.append(s);
+        		
+        	}
     }
 }
